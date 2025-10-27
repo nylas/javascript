b9031791b5a08389f6fc99f0c5aaeb6b99eb37a7
Dan Radenkovic <bogdan.radenkovic@gmail.com>
2025-10-27 11:30:24 +0100

    update tooling

--- packages/react/vitest.config.ts
+++ packages/react/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    globals: true,
     environment: "jsdom",
     include: ["lib/**/*.test.ts", "lib/**/*.test.tsx", "lib/**/*.spec.tsx"],
     setupFiles: ["./test/vitest.setup.ts"],
